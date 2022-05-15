d25d54381f2e9bef1dd85cceb55d022086a9e407
pteroz <pteroz@DESKTOP-KIQM67U.localdomain>
2022-05-16 00:33:44 +0300

    Committer: pteroz <pteroz@DESKTOP-KIQM67U.localdomain>  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   brain_games/__pycache__/__init__.cpython-310.pyc
    	new file:   brain_games/cli.py
    	new file:   brain_games/scripts/.brain_games.py.swp
    	modified:   brain_games/scripts/brain_games.py
    	modified:   dist/hexlet-code-0.1.0.tar.gz
    	modified:   dist/hexlet_code-0.1.0-py3-none-any.whl
    	modified:   poetry.lock
    	modified:   pyproject.toml

---
+++ brain_games/cli.py
@@ -0,0 +1,5 @@
+import prompt
+
+def welcome_user():
+    name = ('May I have you name?')
+    print('Hello, John!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,8 @@
+from ../cli import welcome_user
+
 def main():
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__ == '__main__':
     main()
